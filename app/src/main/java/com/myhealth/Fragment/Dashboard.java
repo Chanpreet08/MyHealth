e5e20a4b0a8380f405dae5368aaa926bfe3c27a5
Chanpreet08 <chanpreet.chhabra@gmail.com>
2017-04-10 23:59:32 +0530

    JSON form

--- app/src/main/java/com/myhealth/Fragment/Dashboard.java
+++ app/src/main/java/com/myhealth/Fragment/Dashboard.java
@@ -6,25 +6,76 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.myhealth.R;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class Dashboard extends Fragment {
+
     private String dailyActivityJson;
     private String getWeightJson;
-
+    private TextView weight;
+    private TextView step;
+    private TextView goalWeight;
+    private TextView goalStep;
+    private ArrayList<HashMap<String,String>> weightJson = new ArrayList<>();
+    private ArrayList<HashMap<String,String >> dailyActivityList = new ArrayList<>();
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v= inflater.inflate(R.layout.fragment_dashboard, container, false);
+        weight=(TextView)v.findViewById(R.id.weight);
+        step=(TextView)v.findViewById(R.id.step);
+        goalWeight=(TextView)v.findViewById(R.id.goal_weight);
+        goalStep=(TextView)v.findViewById(R.id.goal_step);
         dailyActivityJson=loadJSON("getDailyActivity.json");
         getWeightJson=loadJSON("getWeight.json");
+        try{
+            JSONObject weight = new JSONObject(getWeightJson);
+            JSONArray weightArray = weight.getJSONArray("weight");
+            HashMap<String,String> map = new HashMap<>();
+            for(int i=0;i<weightArray.length();i++)
+            {
+                JSONObject inside = weightArray.getJSONObject(i);
+                String wt = inside.getString("weight");
+                map.put("weight",wt);
+                weightJson.add(map);
+            }
+        }
+        catch(JSONException e)
+        {
+            e.printStackTrace();
+        }
+        try{
+            JSONObject object = new JSONObject(dailyActivityJson);
+            JSONArray activityArray = object.getJSONArray("activities");
+            HashMap<String,String> map = new HashMap<>();
+            for(int i=0;i<activityArray.length();i++)
+            {
+                JSONObject inside = activityArray.getJSONObject(i);
+                String step = inside.getString("steps");
+                map.put("steps",step);
+                dailyActivityList.add(map);
+            }
+            JSONObject goals = object.getJSONObject("goals");
+        }
+        catch(JSONException e)
+        {
+            e.printStackTrace();
+        }
+        weight.setText(weightJson.get(0).get("weight"));
         return v;
     }
 
